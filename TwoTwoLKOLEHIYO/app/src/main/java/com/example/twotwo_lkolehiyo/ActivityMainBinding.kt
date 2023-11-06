4627da80ab3ee7d96af16f36c7897ebdb6e57e69
S312User <S312User@S312.uc-bcf.edu.ph>
2023-11-06 18:42:35 +0800

    application

---
+++ TwoTwoLKOLEHIYO/app/src/main/java/com/example/twotwo_lkolehiyo/ActivityMainBinding.kt
@@ -0,0 +1,12 @@
+package com.example.twotwo_lkolehiyo
+
+class ActivityMainBinding {
+    val password: Any
+
+    companion object {
+        fun inflate(layoutInflater: LayoutInflater): ActivityMainBinding {
+
+        }
+    }
+
+}
---
+++ TwoTwoLKOLEHIYO/app/src/main/java/com/example/twotwo_lkolehiyo/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.twotwo_lkolehiyo
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.loginapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener(View.OnClickListener {
+            if (binding.username.text.toString() == "user" && binding.password.text.toString() == "1234"){
+                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
