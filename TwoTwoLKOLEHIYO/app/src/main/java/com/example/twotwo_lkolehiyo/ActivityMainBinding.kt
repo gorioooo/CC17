b9b39c179140a44c34b8145ed36632921cd324dd
S312User <S312User@S312.uc-bcf.edu.ph>
2023-11-08 19:01:53 +0800

    updated

--- TwoTwoLKOLEHIYO/app/src/main/java/com/example/twotwo_lkolehiyo/ActivityMainBinding.kt
+++ TwoTwoLKOLEHIYO/app/src/main/java/com/example/twotwo_lkolehiyo/ActivityMainBinding.kt
@@ -1,10 +1,13 @@
 package com.example.twotwo_lkolehiyo
 
+import android.view.LayoutInflater
+
 class ActivityMainBinding {
-    val password: Any
+    val root: Any = TODO()
+    val password: Any = TODO()
 
     companion object {
-        fun inflate(layoutInflater: LayoutInflater): ActivityMainBinding {
+        fun inflate(layoutInflater: LayoutInflater) {
 
         }
     }
--- TwoTwoLKOLEHIYO/app/src/main/java/com/example/twotwo_lkolehiyo/MainActivity.kt
+++ TwoTwoLKOLEHIYO/app/src/main/java/com/example/twotwo_lkolehiyo/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.twotwo_lkolehiyo
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.loginapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,14 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView()
 
-        binding.loginButton.setOnClickListener(View.OnClickListener {
-            if (binding.username.text.toString() == "user" && binding.password.text.toString() == "1234"){
-                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
-            }
-        })
+        setOnClickListener()
     }
+
+    private fun setContentView() {
+        TODO("Not yet implemented")
+    }
+}
+
+private fun setOnClickListener() {
+    TODO("Not yet implemented")
 }
