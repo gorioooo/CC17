204c2fc6a4cc425c940bebb13af7cd0a5c450fa6
November E. Nicomedez <november16nn@gmail.com>
2023-12-10 21:13:16 +0800

    feat added features

--- TwoTwoLKOLEHIYO2/app/src/main/java/com/example/twotwo_lkolehiyo/MainActivity.java
+++ TwoTwoLKOLEHIYO2/app/src/main/java/com/example/twotwo_lkolehiyo/MainActivity.java
@@ -1,14 +1,2 @@
-package com.example.twotwo_lkolehiyo;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+package com.example.twotwo_lkolehiyo;public class MainActivity {
 }
